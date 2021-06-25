200995aee867fc0133efdfdcf91dcb4bf456ce32
Calo <calo@estudiantec.cr>
2021-06-24 20:28:48 -0600

    Se empezó a trabajar la tabla de símbolos

--- test.c
+++ test.c
@@ -1,46 +1,6 @@
 int a;
-int a[];
-int sumar(int a){
-	a = 1;
-	a = {1,2-(f/45),3,4+5};
-	read();
-	cuarenta = 20 + 20;
-	i = 20+(2*f);
-	
-	if (a>0) {
-		return
-	}
-	return a;
-	
-	while (a++) 
-	{ 
-		b+2;
-		a = 2+4;
-	}
-	do a=1; while (i+2);
-	
-	if (a>1) read(a); else write(A);
-	
-	if (a>1){
-		a = a+2;
-	} else {
-		continue;
-	}	
-	
-	switch (a)
-	{
-		case 1:
-			break;
-		case 2:
-			break;
-		default: 
-			break;
-	}
-	
-	for (i = 1; i < 11; ++i)
-	{
-		read();
-	}
 
-	if (a == 1) if (a == 2) a--; else a++; else a--;
+void main(int x, char c, int f){
+	int e;
+	f = 5;
 }
