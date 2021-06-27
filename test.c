2b1a824bffaf99258a6ba60199fbdc10666f0835
Calo <calo@estudiantec.cr>
2021-06-26 18:48:34 -0600

    Analisis semantico (casi) listo

--- test.c
+++ test.c
@@ -1,6 +1,25 @@
-int x;
-int y;
+int x, y;
+
+int hola(int a){
+	x++;
+}
  
 int main(int a, int b, int c){
-  y = x;
- }
+	y = x;
+	break;
+	continue;
+	
+	while ( x != 10) {
+	  y= x + 1;
+	  x++;
+	  continue;
+	  break;
+	}
+	
+	do {
+		x++;
+		continue;
+		break;
+	}
+	while(x>2);
+}
